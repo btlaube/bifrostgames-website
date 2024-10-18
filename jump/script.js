2e07c21591bbfef64853e14cda8fc32e1a912532
btlaube <69732464+btlaube@users.noreply.github.com>
2024-10-18 17:16:51 -0500

    jump game 0.1

--- jump/script.js
+++ jump/script.js
@@ -6,7 +6,8 @@ var modalText = document.getElementById("modalText");
 var restartButton = document.getElementById("restartButton");
 var quitButton = document.getElementById("quitButton");
 
-function jump(){
+function jump()
+{
     if(character.classList.contains("animate")){ return; }
     character.classList.add("animate");
     setTimeout(function(){
@@ -14,33 +15,52 @@ function jump(){
     }, 300);
 }
 
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft < 20 && blockLeft > -20 && characterTop >= 130){
-        block.style.animation = "none";
-        showModal();
-    } else {
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
-    }
-}, 10);
+function showStartModal()
+{
+    startModal.style.display = "block";
+}
 
-function showModal() {
+startButton.onclick = function()
+{
+    startModal.style.display = "none";
+    startGame();
+};
+
+// Function to start the game
+function startGame()
+{
+    counter = 0;
+    block.style.animation = "block 1s infinite linear";
+}
+
+var checkDead = setInterval(function()
+    {
+        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        if(blockLeft < 20 && blockLeft > -20 && characterTop >= 130){
+            block.style.animation = "none";
+            showModal();
+        } else {
+            counter++;
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+        }
+    }, 10);
+
+function showModal()
+{
     modal.style.display = "block";
     modalText.innerHTML = "Game Over. Score: " + Math.floor(counter / 100);
 }
 
 // Close modal and restart game
-restartButton.onclick = function() {
+restartButton.onclick = function()
+{
     modal.style.display = "none";
-    counter = 0;
-    block.style.animation = "block 1s infinite linear";
+    startGame();
 };
 
-// Quit game (for demo purposes, you can customize what "Quit" does)
-quitButton.onclick = function() {
+quitButton.onclick = function()
+{
     modal.style.display = "none";
     alert("Thanks for playing!"); 
-    // You could redirect or perform another action here
 };
