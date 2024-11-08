1e6896a855b6bd53717fe9e46898dc58edc17afc
btlaube <69732464+btlaube@users.noreply.github.com>
2024-11-08 12:04:11 -0600

    added stuff

---
+++ assets/js/clicker-script.js
@@ -0,0 +1,76 @@
+class PlayerData
+{
+    constructor()
+    {
+        this.inventory = new Inventory();
+    }
+    
+    getInventory()
+    {
+        return this.inventory;
+    }
+
+    collectItem(item, quantity)
+    {
+        this.inventory.addItem(item, quantity);
+    }
+
+    spendItem(item, quantity)
+    {
+        if (this.inventory.hasItem(item) && this.inventory.getItemQuantity(item) >= quantity)
+            this.inventory.removeItem(item, quantity);
+    }
+
+    outputData()
+    {
+        console.log(`${this.inventory}`);
+    }
+
+}
+
+class Inventory
+{
+    constructor()
+    {
+        inventoryItems = {};
+    }
+
+    hasItem(item)
+    {
+        return (this.inventoryItems[item]);
+    }
+
+    getItemQuantity(item)
+    {
+        if (inventoryItems[item])
+            return (this.inventoryItems[item]);
+    }
+
+    addItem(item, quantity)
+    {
+        if(inventoryItems[item])
+        {
+            inventoryItems[item] += quantity;
+        }
+        else
+        {
+            inventoryItems[item] = quantity;
+        }
+    }
+
+    removeItem(item, quantity)
+    {
+        if(inventoryItems[item])
+        {
+            inventoryItems[item] -= quantity;
+        }
+    }
+}
+
+
+var player = new PlayerData();
+player.outputData();
+player.collectItem("Stone", 100);
+player.outputData();
+player.spendItem("Stone", 50);
+player.outputData();
